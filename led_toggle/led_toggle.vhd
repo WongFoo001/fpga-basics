58d866ad9ec7d8a4b93a64bc4b4685984ab3f807
Justin Wong <wong.justin00@gmail.com>
2025-11-25 18:50:46 -0800

    fix indentation in led_toggle

--- led_toggle/led_toggle.vhd
+++ led_toggle/led_toggle.vhd
@@ -2,12 +2,12 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity led_toggle is
-  port (
-    -- switch enable, no debounce since this is just a simple example
-    en  : in  std_logic;
-    -- output to drive led
-    led : out std_logic
-  );
+port (
+  -- switch enable, no debounce since this is just a simple example
+  en  : in  std_logic;
+  -- output to drive led
+  led : out std_logic
+);
 end led_toggle;
 
 architecture rtl of led_toggle is
