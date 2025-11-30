1c51eb6964a43034a9ad66c609f706ac882febc0
Justin Wong <wong.justin00@gmail.com>
2025-11-29 19:54:08 -0800

    create basic testbench template

---
+++ utilities/testbench_template.sv
@@ -0,0 +1,36 @@
+module bad_typewriter_tb;
+  localparam CLK_HALF_PERIOD_NS = 5;
+  logic clk, resetn;
+
+  // TODO: custom port declarations here
+
+  // TODO: DUT instantiation here
+
+  // clock generation
+  initial begin
+    clk = 1;
+
+    forever begin
+      #(CLK_HALF_PERIOD_NS * 1ns); clk = !clk;
+    end
+  end
+
+  // stimulus generation
+  initial begin
+    do_reset();
+
+    // TODO: custom stimulus here
+  end
+
+  task wait_num_clocks(int unsigned num);
+    repeat(num) #(2 * CLK_HALF_PERIOD_NS * 1ns);
+  endtask
+
+  task do_reset();
+    resetn = 0;
+    wait_num_clocks(1);
+    resetn = 1;
+    wait_num_clocks(1);
+  endtask
+endmodule
+
